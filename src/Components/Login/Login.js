6e49a5e2a6a994a4701ed66131ec980dae5f27e8
Ashwin S <ashwin94429@gmail.com>
2024-09-27 12:21:53 +0530

    Added email service for resetting password

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Login.css";
 import loginImage from "../../Assets/Computer login-rafiki.png";
 import axios from "axios";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 
@@ -13,6 +13,8 @@ const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isResetAvailable, setIsResetAvailable] = useState(false);
+  const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,8 +50,11 @@ const Login = () => {
       const userId = decodedToken.nameid;
       console.log(userId);
     } catch (error) {
-      if (error.response && (error.response.status === 400 || error.response.status === 401)) {
-        nav("/not-authorized"); 
+      if (
+        error.response &&
+        (error.response.status === 400 || error.response.status === 401)
+      ) {
+        nav("/not-authorized");
       } else {
         console.log("An error occurred", error);
       }
@@ -60,50 +65,95 @@ const Login = () => {
     setError("");
   };
 
+  const handleEmail = (value) => {
+    setEmail(value);
+  };
+
+  const handlePasswordReset = () => {
+    setIsResetAvailable(!isResetAvailable);
+    console.log(isResetAvailable);
+    
+  };
+  console.log(isResetAvailable);
+
+  const handleResetEmail =async ()=>{
+    console.log(email);
+    
+      try {
+        await axios.post("https://localhost:7063/api/Password/ForgotPassword/forgot-password",{email} );
+        alert("Password reset email sent!");
+      } catch (error) {
+        console.error(error);
+      }
+  }
+
   return (
-    <MDBContainer  >
-    <MDBRow className="login-container">
-      <center>
-        {showAlert && <div className="custom-alert">Welcome {userName}</div>}
-      </center>
-      <MDBCol md="6" className="image-section">
-        <img src={loginImage} alt="Login Visual" className="login-image" />
-      </MDBCol>
-      <MDBCol md="6" className="form-section">
-        <h2>Login</h2>
-        {error && <p className="error-message">{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>User Name</label>
-            <input
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              placeholder="Enter your UserName"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <button type="submit" className="login-button">
-            Login
-          </button>
-        </form>
-        <p className="text-center">
-           Don't have an account?{" "}
-          <Link to="/SignUP">Register</Link> {/* Link to signup page */}
-       </p>
-      </MDBCol>
-    </MDBRow>
-  </MDBContainer>
+    <MDBContainer>
+      <MDBRow className="login-container">
+        <center>
+          {showAlert && <div className="custom-alert">Welcome {userName}</div>}
+        </center>
+        <MDBCol md="6" className="image-section">
+          <img src={loginImage} alt="Login Visual" className="login-image" />
+        </MDBCol>
+        <MDBCol md="6" className="form-section">
+          <h2>Login</h2>
+          {error && <p className="error-message">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            {isResetAvailable == false ? (
+              <div>
+                <div className="form-group">
+                  <label>User Name</label>
+                  <input
+                    type="text"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    placeholder="Enter your UserName"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+                <button type="submit" className="login-button">
+              Login
+            </button>   
+              </div>
+              
+            ) : (
+              <div className="form-group">
+                <label>Reset Email</label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your Email"
+                  required
+                />
+                <button type="button" onClick={handleResetEmail}>Sent reset email</button>
+              </div>
+            )}
+            
+            <p className="text-center">
+              Forgot Password?{" "}
+              <a onClick={handlePasswordReset}>Reset Password</a>{" "}
+              {/* Link to signup page */}
+            </p>
+          </form>
+          <p className="text-center">
+            Don't have an account? <Link to="/SignUP">Register</Link>{" "}
+            {/* Link to signup page */}
+          </p>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
